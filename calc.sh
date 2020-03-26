18e6289ff5bf204ec29cac4d0e2e9f360b86f151
Manal solanki <manalsolanki@Manals-MacBook-Air.local>
2020-03-26 17:09:29 -0400

    Updated the Exit condition.

--- calc.sh
+++ calc.sh
@@ -29,47 +29,55 @@ echo "5.Exit"
 
 # Then, capture the user selection.
 
-read option
-echo "**********************************"
-echo "Enter Two numbers "
-read num_1
-read num_2
-# If the selection matches a supported operation, execute the operation.
+while true; do
+
+  read option
+  echo "**********************************"
+  echo "Enter Two numbers "
+  read num_1
+  read num_2
+  # If the selection matches a supported operation, execute the operation.
 
 
-case $option in 
-    1 )
-      echo "------------------------"
-      echo "Addition of $num_1 and $num_2 is"
-      expr $num_1 + $num_2
-    ;;
+  case $option in 
+      1 )
+        echo "------------------------"
+        echo "Addition of $num_1 and $num_2 is"
+        expr $num_1 + $num_2
+      ;;
 
-    2 )
-      echo "------------------------"
-      echo "Subtraction of $num_1 and $num_2 is"
-      expr $num_1 - $num_2
-    ;;
+      2 )
+        echo "------------------------"
+        echo "Subtraction of $num_1 and $num_2 is"
+        expr $num_1 - $num_2
+      ;;
 
-    3 )
-      echo "------------------------"
-      echo "Multiplication of $num_1 and $num_2 is"
-      expr $num_1 \* $num_2
-    ;;
+      3 )
+        echo "------------------------"
+        echo "Multiplication of $num_1 and $num_2 is"
+        expr $num_1 \* $num_2
+      ;;
 
-    4 )
-      echo "------------------------"
-      if [[ $num_2 -eq 0 ]]; then
-      echo  "Division not possible because denominator is 0"
-      else
-      echo "Division of $num_1 and $num_2 is"
-      expr $num_1 / $num_2
-    fi
-    ;;
-# If the selection does not match a support operation, display an error message.
-    *)
-      echo "------------------------"
-      echo "Please select a appropriate choice"
-    ;;
-esac
+      4 )
+        echo "------------------------"
+        if [[ $num_2 -eq 0 ]]; then
+        echo  "Division not possible because denominator is 0"
+        else
+        echo "Division of $num_1 and $num_2 is"
+        expr $num_1 / $num_2
+      fi
+      ;;
 
+      5 )
+        echo "------------------------"
+        echo "Thanks for using this operations"
+        exit 0
+        ;;
+
+      * )
+        echo "------------------------"
+        echo "Please select a appropriate choice"
+      ;;
+  esac
+done
 # When the operation is complete, redisplay the menu.
