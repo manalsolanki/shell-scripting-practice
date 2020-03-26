8b4fb9fa82355526e2f2f9ac2073ce402a121b87
Manal solanki <manalsolanki@Manals-MacBook-Air.local>
2020-03-26 14:53:43 -0400

    Displayed the menu and took user option.

--- calc.sh
+++ calc.sh
@@ -19,7 +19,18 @@ echo $greeting
   # Add
   # Subtract
   # Exit
+
+echo "Please select the following number for appropriate operation."
+echo "1.Add"
+echo "2.Subtract"
+echo "3.Multiply"
+echo "4.Divide"
+echo "5.Expotential"
+
 # Then, capture the user selection.
+
+read $option
+
 # If the selection matches a supported operation, execute the operation.
 # If the selection does not match a support operation, display an error message.
 # When the operation is complete, redisplay the menu.
