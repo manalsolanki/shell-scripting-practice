3d8edb1bdf3407417e3e76c7da7e447bbd2106da
Manal Solanki <60120657+manalsolanki@users.noreply.github.com>
2020-03-26 17:11:51 -0400

    Operations for each selection (#7)
    * Add the introductory greeting message according to time.
    
    * Displayed the menu and took user option.
    
    * Updated the operation for each menu.
    
    * Entered the operation for invalid selection.
    
    * Updated the Exit condition.
    
    Co-authored-by: Manal solanki <manalsolanki@Manals-MacBook-Air.local>

--- calc.sh
+++ calc.sh
@@ -25,12 +25,60 @@ echo "1.Add"
 echo "2.Subtract"
 echo "3.Multiply"
 echo "4.Divide"
-echo "5.Expotential"
+echo "5.Exit"
 
-# Then, capture the user selection.
+# Then, capture the user selection
 
-read $option
+  read option
+  echo "**********************************"
+  echo "Enter Two numbers "
+  read num_1
+  read num_2
+  # If the selection matches a supported operation, execute the operation.
+
+
+  case $option in 
+      1 )
+        echo "------------------------"
+        echo "Addition of $num_1 and $num_2 is"
+        expr $num_1 + $num_2
+      ;;
+
+      2 )
+        echo "------------------------"
+        echo "Subtraction of $num_1 and $num_2 is"
+        expr $num_1 - $num_2
+      ;;
+
+      3 )
+        echo "------------------------"
+        echo "Multiplication of $num_1 and $num_2 is"
+        expr $num_1 \* $num_2
+      ;;
+
+      4 )
+        echo "------------------------"
+        if [[ $num_2 -eq 0 ]]; then
+        echo  "Division not possible because denominator is 0"
+        else
+        echo "Division of $num_1 and $num_2 is"
+        expr $num_1 / $num_2
+      fi
+      ;;
+
+      5 )
+        echo "------------------------"
+        echo "Thanks for using this operations"
+        exit 0
+        ;;
+
+      * )
+        echo "------------------------"
+        echo "Please select a appropriate choice"
+      ;;
+  esac
+# When the operation is complete, redisplay the menu
 
 # If the selection matches a supported operation, execute the operation.
 # If the selection does not match a support operation, display an error message.
-# When the operation is complete, redisplay the menu.
+# When the operation is complete, redisplay the menu
